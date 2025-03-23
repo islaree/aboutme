508230cc75c5ae2c88d0b55606d2ae3774cff498
daxchx <daxchx.v1@gmail.com>
2025-03-23 21:02:36 +0900

    fix: ビルドエラーを修正 (#1)

--- app/page.tsx
+++ app/page.tsx
@@ -1,37 +1,27 @@
 export default function Home() {
   return (
     <div>
-      <p>👋Hi, I'm islaree.</p>
+      <p>👋Hi, I&apos;m islaree.</p>
       <div>
         <h2>## bio</h2>
         <ul>
           <li>
             <h3>20xx: example.inc</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro
-              provident neque debitis deserunt modi iste cumque exercitationem
-              asperiores laborum saepe, unde earum repellendus necessitatibus
-              qui autem beatae aliquid quibusdam? Harum.
-            </p>
+            <p>bio text</p>
           </li>
           <li>
             <h3>20xx: example.inc</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro
-              provident neque debitis deserunt modi iste cumque exercitationem
-              asperiores laborum saepe, unde earum repellendus necessitatibus
-              qui autem beatae aliquid quibusdam? Harum.
-            </p>
+            <p>bio text</p>
           </li>
         </ul>
       </div>
       <h2>## blog</h2>
       <ul>
         <li>
-          <a href="/">blog title1</a>
+          <a href="https://github.com/islaree/aboutme">blog title1</a>
         </li>
         <li>
-          <a href="/">blog title2</a>
+          <a href="https://github.com/islaree/aboutme">blog title2</a>
         </li>
       </ul>
     </div>
