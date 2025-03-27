4739ad37fc4cbfb57ab13bd0ca3c7fb594c41a3c
daxchx <daxchx.v1@gmail.com>
2025-03-27 23:22:57 +0900

    feat: 記事表示をssrで追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,7 @@
-"use client";
-
-import { useState, useEffect } from "react";
-
-type User = {
-  id: number;
-  username: string;
-  name: string;
-  avatar_small_url: string;
-};
-
-type Article = {
-  id: number;
-  post_type: string;
-  title: string;
-  slug: string;
-  comments_count: number;
-  liked_count: number;
-  bookmarked_count: number;
-  body_letters_count: number;
-  article_type: string;
-  emoji: string;
-  is_suspending_private: boolean;
-  published_at: string;
-  body_updated_at: string;
-  source_repo_updated_at: null;
-  pinned: boolean;
-  path: string;
-  user: User;
-  publication: null;
-};
+import { Suspense } from "react";
+import { Posts } from "./components/posts";
 
 export default function Home() {
-  const [posts, setPosts] = useState<Article[]>([]);
-
-  useEffect(() => {
-    const getPost = async () => {
-      const res = await fetch("/api/blog");
-      if (!res.ok) return;
-      const { data } = await res.json();
-      setPosts(data.articles);
-    };
-    getPost();
-  }, []);
-
   return (
     <div>
       <p>👋Hi, I&apos;m islaree.</p>
@@ -60,19 +19,9 @@ export default function Home() {
         </ul>
       </div>
       <h2>## blog</h2>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <a
-              href={`https://zenn.dev${post.path}`}
-              target="_blank"
-              className="underline"
-            >
-              {post.title}
-            </a>
-          </li>
-        ))}
-      </ul>
+      <Suspense fallback={<p>...loading</p>}>
+        <Posts />
+      </Suspense>
     </div>
   );
 }
